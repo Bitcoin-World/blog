509fe0b7ac57025ab21d558d1fcea18707c6ea95
Bitcoin-World <offshorRF@gmail.com>
2021-01-27 13:38:14 +0000

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Tag, Post
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form_control'}),
+            'slug': forms.TextInput(attrs={'class': 'form_control'}),
+
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError('Slug must be unique. We have "{}" slug already'.format(new_slug))
+        return new_slug
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'from-control'}),
+            'slug': forms.TextInput(attrs={'class': 'from-control'}),
+            'body': forms.Textarea(attrs={'class': 'from-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'from-control'}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        return new_slug
